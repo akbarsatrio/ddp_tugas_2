75b78d162205a2e8d25b0270bd4b9bf4437225ef
akbarsatrio <akbarsatrio@outlook.co.id>
2021-01-12 10:28:29 +0700

    edit transfer dan buka rekening

--- app/bukaRekening.py
+++ app/bukaRekening.py
@@ -8,30 +8,14 @@ import app.tarikTunai as tarikTunai
 import app.transfer as transfer
 
 def main():
-  # # Masukan Nama Nasabah
-  # namaNasabah = input('Masukan Nama Nasabah : ')
-  # # Masukan Setoran Awal
-  # setoranAwal = input('Masukan Setoran Awal : Rp. ')
-  # # Generate nomor rekening lewat random string
-  # norekNasabah = "REK" + ''.join(random.choice(string.digits) for _ in range(3))
-  # pinNasabah = getpass.getpass('Masukan PIN : ')
-  # # simpan ketiga variabel ke dalam variabel data
-  # data = (norekNasabah+','+namaNasabah+','+setoranAwal+','+pinNasabah+'\n')
-  # # buka file nasabah.txt dan simpan kedalam variabel file dengan permission 'a'
-  # file = open('data/nasabah.txt', 'a')
-  # # panggil variabel file lalu tulis menggunakan fungsi writelines dengan value variabel data
-  # file.writelines(data)
-  # # tutup jika penulisan data sudah selesai
-  # file.close()
-  # print('Pembuatan Rekening baru berhasil')
   print('\n*** BUKA REKENING ***')
   namaNasabah = input('Masukan nama : ')  #input untuk memasukan Nama
   while True: #perulangan tanpa henti jika pengguna memasukan input yang nilainya false
     print('[ PIN HARUS 6 DIGIT ]')
-    pinNasabah = getpass.getpass('Buat PIN Anda : ')
-    if len(pinNasabah) == 6:
-      pinNasabah2 = getpass.getpass('Konfirmasi PIN Anda : ')
-      if pinNasabah == pinNasabah2:
+    pinNasabah = getpass.getpass('Buat PIN Anda : ')  #buat pin nasabah dengan fungsi getpass
+    if len(pinNasabah) == 6:  #jika pin nasabah panjangnya adalah 6, maka jalankan statement dibawah
+      pinNasabah2 = getpass.getpass('Konfirmasi PIN Anda : ') #buat konfirmasi pin
+      if pinNasabah == pinNasabah2: #jika pin nasabah valuenya sama dengan pinnasabah2
         setoranNasabah = int(input('Masukan setoran awal : Rp. '))  #input untuk seotran nasabah
         if setoranNasabah >= 10000: #percabangan untuk memvalidasi harus ada minimul setoran yaitu 10rb
           norekNasabah = 'REK'+''.join(random.choice(string.digits) for _ in range(3))  #generate nomor otomatis menggunakan random string
--- app/transfer.py
+++ app/transfer.py
@@ -9,30 +9,30 @@ import app.transfer as transfer
 
 def main():
   print('*** TRANSFER ***')
-  while True:
-    norekNasabah = input('Masukan nomor rekening Anda : ')
-    pinNasabah = getpass.getpass('Masukan PIN : ')
-    if checkNorek(norekNasabah, pinNasabah) is True:
-      while True:
+  while True: #menjalankan perulangan dengan kondisi True
+    norekNasabah = input('Masukan nomor rekening Anda : ')  #memasukan nomor rekening sumber/nasabah
+    pinNasabah = getpass.getpass('Masukan PIN : ')  #memasukan pin rekening sumber/nasabah
+    if checkNorek(norekNasabah, pinNasabah) is True:  #panggil fungsi checknorek dengan parameter norek nasabah dan pin nasabah
+      while True: #jika fungsi diatas mengembalikan nilai true, maka jalankan perulangan while ke-2 dengan kondisi True
         print()
-        norekTujuan = input('Masukan nomor rekening tujuan : ')
-        if checkNorekTujuan(norekTujuan) is True:
-          nominalTransfer = int(input('Masukan nominal yang akan di transfer : Rp. '))
-          if int(checkNorek.getNasabahDataSaldo) >= nominalTransfer:
-            totalUangNasabah = int(checkNorek.getNasabahDataSaldo)-nominalTransfer
-            totalUangTujuan = int(checkNorekTujuan.getTujuanDataSaldo)+nominalTransfer
-            currentDataNasabah = [norekNasabah, checkNorek.getNasabahDataNama, str(totalUangNasabah), pinNasabah+'\n']
-            currentDataTujuan = [norekTujuan, checkNorekTujuan.getTujuanNama, str(totalUangTujuan), checkNorekTujuan.getTujuanPin]
-            checkNorek.getNasabahData[checkNorek.getNasabahDataIndex] = ','.join(currentDataNasabah)
-            checkNorek.getNasabahData[checkNorekTujuan.getTujuanDataIndex] = ','.join(currentDataTujuan)
-            openNasabahWrite = open('data/nasabah.txt', 'w')
-            for dataInput in checkNorek.getNasabahData:
-              openNasabahWrite.writelines(dataInput)
-            openNasabahWrite.close()
-            openTransfer = open('data/transfer.txt', 'a')
-            dataTransfer = ['TRF'+''.join(random.choice(string.digits) for _ in range(3)), norekNasabah, norekTujuan, str(nominalTransfer)]
-            openTransfer.writelines(','.join(dataTransfer))
-            openTransfer.close()
+        norekTujuan = input('Masukan nomor rekening tujuan : ') #input norek tujuan
+        if checkNorekTujuan(norekTujuan) is True: #panggil funsgi check norek tujuan dan jika mengembalikan nilai true, maka...
+          nominalTransfer = int(input('Masukan nominal yang akan di transfer : Rp. '))  #masukan nominal transfer
+          if int(checkNorek.getNasabahSaldo) >= nominalTransfer:  #ambil data saldo nasabah di fungsi check norek
+            totalUangNasabah = int(checkNorek.getNasabahSaldo)-nominalTransfer  #kurangi data saldo nasabah dengan nominal jumlah transfer lalu masukan kedalam total uang nasabah
+            totalUangTujuan = int(checkNorekTujuan.getTujuanDataSaldo)+nominalTransfer  #tambahkan data saldo tujuan dengan nominal transfer lalu simpan kedalam total uang tujuan
+            currentDataNasabah = [norekNasabah, checkNorek.getNasabahNama, str(totalUangNasabah), pinNasabah+'\n']  #buat data nasabah sumber yang baru/diubah dan simpan kedalam variabel currentDataNasabah dengan tipe data list
+            currentDataTujuan = [norekTujuan, checkNorekTujuan.getTujuanNama, str(totalUangTujuan), checkNorekTujuan.getTujuanPin]  #buat data nasbaah tujuan yang baru/diubah dan simpan kedalam variabel currentDataTUjuan dengan tipe data list
+            checkNorek.getNasabahData[checkNorek.getNasabahIndex] = ','.join(currentDataNasabah)  #panggil variabel getNasabahData di fungsi checkNorek serta index nasabah sumber pada checkNorek.getNasabahIndex, lalu valuenya diganti dengan currentDataNasabah
+            checkNorek.getNasabahData[checkNorekTujuan.getTujuanIndex] = ','.join(currentDataTujuan)  #panggil variabel getNasabahData di fungsi checkNorek serta index nasabah tujuan pada checkNorekTujuan.getTujuanIndex, lalu valuenya diganti dengan currentDataTujuan
+            openNasabahWrite = open('data/nasabah.txt', 'w')  #buka file nasabah txt dengan permission write lalu simpan kedalam openNasabahWrite
+            for dataInput in checkNorek.getNasabahData: #loop value dari checkNorek.getNasabahData sebagai variabel dataInput
+              openNasabahWrite.writelines(dataInput)  #panggil variabel openNasabahWrite lalu tulis dengan fungsi writelines dengan value dataInput, otomatis file nasabah.txt akan ditimpa semuanya dengan dataInput
+            openNasabahWrite.close()  #tutup file jika aksi sudah selesai
+            openTransfer = open('data/transfer.txt', 'a') #sekarang buka file transfer, dengan permisssion file 'a'
+            dataTransfer = ['TRF'+''.join(random.choice(string.digits) for _ in range(3)), norekNasabah, norekTujuan, str(nominalTransfer)+'\n']  #buat dataTransfer ddengan value kode unik transfer, norek nasabah, norek tujuan, nominal transfer
+            openTransfer.writelines(','.join(dataTransfer)) #tulis data transfer ke openTransfer menggunakan fungsi writelines
+            openTransfer.close()  #tutup jika aksi sudah selesai
             print('Transfer berhasil, total saldo Anda sekarang Rp.', totalUangNasabah)
             print('Tekan Enter untuk ke menu')
             input()
@@ -50,34 +50,37 @@ def main():
       print('PIN atau nomor rekening Anda salah')
       pass
 
-def checkNorek(norekNasabah, pinNasabah):
-  openNasabah = open('data/nasabah.txt')
-  checkNorek.getNasabahData = openNasabah.readlines()
-  nasabahDataList = []
-  checkNorek.getNasabahDataIndex = 0
-  checkNorek.getNasabahDataNama = ''
-  checkNorek.getNasabahDataSaldo = 0
-  checkNorek.getAllNasabah = []
-  for nasabahData in checkNorek.getNasabahData:
-    nasabahData = nasabahData.split(',')
-    if nasabahData[0] == norekNasabah:
-      if nasabahData[3] == pinNasabah+'\n':
-        checkNorek.getNasabahDataNama = nasabahData[1]
-        checkNorek.getNasabahDataSaldo = nasabahData[2]
+def checkNorek(norekNasabah, pinNasabah): #definisikan checkNorek dengan parameter norekNasabah dan pinNasabah
+  # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+  # nama variabel yang diawali dengan nama fungsi itu sendiri agar variabelnya bisa dipanggil di fungsi lain  #
+  # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+  openNasabah = open('data/nasabah.txt')  #buka file nasabah.txt dan simpan kedalam openNasabah
+  checkNorek.getNasabahData = openNasabah.readlines() #buat variabel getNasabahData dengan value openNasabah.readlines()
+  checkNorek.getNasabahIndex = 0  #ini untuk menampung index ke berapa dalam data nasabah (untuk foreign key norek sumber)
+  checkNorek.getNasabahNama = ''  #ini untuk menampung nama nasabah sumber
+  checkNorek.getNasabahSaldo = 0  #ini untuk menampung nasbaah saldo
+  for nasabahData in checkNorek.getNasabahData: #loop value dari getNasabahData sebagai nasabahData
+    nasabahData = nasabahData.split(',')  #variabel nasabah data di split dengan ',' lalu simpan kedalam variabel itu sendiri
+    if nasabahData[0] == norekNasabah:  #jika nasabah data index 0 (rekening) sama dengan norekNasabah (input), maka...
+      if nasabahData[3] == pinNasabah+'\n': #jika nasabah data index 3 (rekening) sama dengan pinNasabah (input), maka...
+        checkNorek.getNasabahNama = nasabahData[1]  #timpa variabel getNasabahNama dengan nasabahData index 1
+        checkNorek.getNasabahSaldo = nasabahData[2] #timpa variabel getNasabahSaldo dengan nasabahData index 2
         print('Nomor rekening', norekNasabah, 'a/n', nasabahData[1], 'dengan saldo Rp.', nasabahData[2], end="")
         return True
       else:
         pass
     else:
-      checkNorek.getNasabahDataIndex += 1
+      checkNorek.getNasabahIndex += 1
       pass
   openNasabah.close()
 
 def checkNorekTujuan(norekTujuan):
-  openNorekTujuan = open('data/nasabah.txt')
+  # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+  # nama variabel yang diawali dengan nama fungsi itu sendiri agar variabelnya bisa dipanggil di fungsi lain  #
+  # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+  openNorekTujuan = open('data/nasabah.txt') #buka file nasabah.txt dan simpan kedalam openNasabah
   checkNorekTujuan.getNorekTujuanData = openNorekTujuan.readlines()
-  tujuanDataList = []
-  checkNorekTujuan.getTujuanDataIndex = 0
+  checkNorekTujuan.getTujuanIndex = 0
   checkNorekTujuan.getTujuanNama = ''
   checkNorekTujuan.getTujuanDataSaldo = 0
   checkNorekTujuan.getTujuanPin = ''
@@ -90,6 +93,6 @@ def checkNorekTujuan(norekTujuan):
       print('Nomor rekening tujuan a/n', nasabahTujuanData[1])
       return True
     else:
-      checkNorekTujuan.getTujuanDataIndex += 1
+      checkNorekTujuan.getTujuanIndex += 1
       pass
     

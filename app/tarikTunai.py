b8c10e5103cde933e0400308b53856f304537938
akbarsatrio <akbarsatrio@outlook.co.id>
2021-01-12 14:50:48 +0700

    edit yg ada

--- app/tarikTunai.py
+++ app/tarikTunai.py
@@ -7,7 +7,6 @@ import app.tarikTunai as tarikTunai
 import app.transfer as transfer
 
 def main():
-<<<<<<< HEAD
   print('*** TARIK TUNAI ***')
   while True: #perulangan while dengan kondisi True, ini digunakan ketika pengguna memasukan input yang mengeluarkan nilai False, maka akan terus berulang
     norekNasabah = input('Masukan nomor rekening Anda : ')  #norek nasabah untuk memasukan nomor rekening nasabah
@@ -36,35 +35,6 @@ def main():
         else:
           print('Tarik tunai minimal Rp. 50000')
           pass
-=======
-  # This is your main area
-  # nanti untuk baca file txt, cukup panggil open('data/{nama file}.txt')
-  # untuk panggil fungsi di file lain, cukup panggil - namafile.namafungsi()
-  # contoh bukaRekening.main()
-  print('TARIK TUNAI')
-  while True: 
-    norekNasabah = input('Masukan nomor rekening Anda : ')
-    pinNasabah = getpass.getpass ('Masukan PIN Anda : ')
-    if checkNorek(norekNasabah, pinNasabah) is True:
-      print()
-      tarikTunai = int(input('Masukan nominal yang akan ditarik : Rp. '))
-      if tarikTunai >= 50000:
-        totalSaldo = int(checkNorek.getNasabahSaldo)-tarikTunai
-        currentData = [norekNasabah, checkNorek.getNasabahNama, str(totalSaldo), pinNasabah+'\n']
-        checkNorek.getNasabahData[checkNorek.getNasabahIndex] = ','.join(currentData)
-        openNasabahWrite = open('data/nasabah.txt', 'w')
-        for dataInput in checkNorek.getNasabahData:
-          openNasabahWrite.writelines(dataInput)
-        openNasabahWrite.close()
-        print('Tarik tunai berhasil, total saldo Anda sekarang Rp.', totalSaldo)
-        print('Tekan Enter untuk menu')
-        input()
-        index.main()
-        return False
-      else:
-        print('Tarik tunai minimal Rp. 50000')
-        pass
->>>>>>> 40c072d452be4b4e29c71bc64a801aee08318b07
     else:
       print('PIN atau nomor rekening Anda salah')
 
@@ -81,17 +51,10 @@ def checkNorek(norekNasabah, pinNasabah): #definisikan checkNorek dengan paramet
     nasabahData = nasabahData.split(',')  #variabel nasabah data di split dengan ',' lalu simpan kedalam variabel itu sendiri
     if nasabahData[0] == norekNasabah:  #jika nasabah data index 0 (rekening) sama dengan norekNasabah (input), maka...
       if nasabahData[3] == pinNasabah+'\n': #jika nasabah data index 3 (rekening) sama dengan pinNasabah (input), maka...
-<<<<<<< HEAD
         checkNorek.getNasabahNama = nasabahData[1]
         checkNorek.getNasabahSaldo = nasabahData[2]
         print('Nomor rekening', norekNasabah, 'a/n', nasabahData[1], 'dengan saldo Rp.', nasabahData[2], end="")
         return True #kembalikan nilai True
-=======
-        checkNorek.getNasabahNama = nasabahData[1]  #timpa variabel getNasabahNama dengan nasabahData index 1
-        checkNorek.getNasabahSaldo = nasabahData[2] #timpa variabel getNasabahSaldo dengan nasabahData index 2
-        print('Nomor rekening', norekNasabah, 'a/n', nasabahData[1], 'dengan saldo Rp.', nasabahData[2], end="")
-        return True
->>>>>>> 40c072d452be4b4e29c71bc64a801aee08318b07
       else:
         pass
     else:

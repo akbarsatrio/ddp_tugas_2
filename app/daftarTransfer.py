12d5f477f02d9f9259b107d1899cec30375f3d4e
akbarsatrio <akbarsatrio@outlook.co.id>
2021-01-12 14:08:55 +0700

    edit yang ada

--- app/daftarTransfer.py
+++ app/daftarTransfer.py
@@ -7,8 +7,56 @@ import app.tarikTunai as tarikTunai
 import app.transfer as transfer
 
 def main():
-  # This is your main area
-  # nanti untuk baca file txt, cukup panggil open('data/{nama file}.txt')
-  # untuk panggil fungsi di file lain, cukup panggil - namafile.namafungsi()
-  # contoh bukaRekening.main()
-  pass
+  print('*** DATA TRANSFER ***')
+  while True: #perulangan while dengan kondisi True, ini digunakan ketika pengguna memasukan input yang mengeluarkan nilai False, maka akan terus berulang
+    norekNasabah = input('Masukan nomor rekening Anda : ')  #norek nasabah untuk memasukan nomor rekening nasabah
+    pinNasabah = getpass.getpass('Masukan PIN Anda : ') #norek nasabah untuk memasukan pin rekening nasabah
+    if checkNorek(norekNasabah, pinNasabah) is True:  #jika fungsi checkNorek mengembalikan nilai True, maka..
+      if checkTransfer(norekNasabah) is True: #jika fungsi checkTransfer mengembalikan nilai True, maka..
+        print('\nDaftar transfer dari rekening', norekNasabah, ':')
+        for dataTransfer in checkTransfer.getTransferData:  #loop value dari variabel getTransferData yang berada di fungsi checkTransfer lalu ditampung kedalam variabel dataTransfer
+          dataTransferSplit = dataTransfer.split(',') #buat variabel dataTrasnferSplit untuk mengubah kedalam array list
+          if dataTransferSplit[1] == norekNasabah:  #jika dataTransferSplit index ke 1 (No Rekening) nilainya sama dengan value dari variabel norekNasabah, maka...
+            dataTransferResult = dataTransfer.replace(',', ' ') #buat variabel dataTransferResult dengan value dataTransfer yang dimana tanda koma (',') di replace dengan whitespace
+            print(dataTransferResult, end="")
+          else:
+            pass
+        print('\nTekan Enter untuk ke menu')
+        input()
+        index.main()
+        return False
+      else:
+        print('Tidak ada data yang ditampilkan')
+    else:
+      print('PIN atau nomor rekening Anda salah')
+
+def checkNorek(norekNasabah, pinNasabah): #fungsi checkNorek untuk memvalidasi nomor rekening di dalam nasabah.txt
+  # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+  # nama variabel yang diawali dengan nama fungsi itu sendiri agar variabelnya bisa dipanggil di fungsi lain  #
+  # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+  openNasabah = open('data/nasabah.txt')  #buka file nasabah.txt lalu simpan kedalam variabel openNasabah
+  checkNorek.getNasabahData = openNasabah.readlines() #buat variabel getNasabahData dengan value openNasabah.readlines()
+  for nasabahData in checkNorek.getNasabahData: #loop value dari checkNorek.getNasabahData sebagai nasabahData
+    nasabahData = nasabahData.split(',')  #variabel nasabah data di split dengan ',' lalu simpan kedalam variabel itu sendiri
+    if nasabahData[0] == norekNasabah:  #jika nasabah data index 0 (rekening) sama dengan norekNasabah (input), maka...
+      if nasabahData[3] == pinNasabah+'\n': #jika nasabah data index 3 (rekening) sama dengan pinNasabah (input), maka...
+        return True #kembalikan nilai True
+      else:
+        pass
+    else:
+      pass
+  openNasabah.close() #tutup file openNasabah
+
+def checkTransfer(norekNasabah):
+  # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+  # nama variabel yang diawali dengan nama fungsi itu sendiri agar variabelnya bisa dipanggil di fungsi lain  #
+  # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+  openTransfer = open('data/transfer.txt')  #buka file transfer.txt lalu simpan kedalam variabel openTransfer
+  checkTransfer.getTransferData = openTransfer.readlines()  #buat variabel getTransferData dengan value openTransfer.readlines()
+  for transferData in checkTransfer.getTransferData:  #loop value dari checkTransfer.getTransfer sebagai nasabahData
+    transferData = transferData.split(',')  #variabel transfer data di split dengan ',' lalu simpan kedalam variabel itu sendiri
+    if transferData[1] == norekNasabah: #jika transferData index ke 1 (nomor rekening) valuenya sama dengan norekNasabah, maka..
+      return True #kembalikan nilai True
+    else:
+      pass
+  openTransfer.close()  #tutup file openTransfer
--- app/tarikTunai.py
+++ app/tarikTunai.py
@@ -7,8 +7,52 @@ import app.tarikTunai as tarikTunai
 import app.transfer as transfer
 
 def main():
-  # This is your main area
-  # nanti untuk baca file txt, cukup panggil open('data/{nama file}.txt')
-  # untuk panggil fungsi di file lain, cukup panggil - namafile.namafungsi()
-  # contoh bukaRekening.main()
-  pass
+  print('*** TARIK TUNAI ***')
+  while True: #perulangan while dengan kondisi True, ini digunakan ketika pengguna memasukan input yang mengeluarkan nilai False, maka akan terus berulang
+    norekNasabah = input('Masukan nomor rekening Anda : ')  #norek nasabah untuk memasukan nomor rekening nasabah
+    pinNasabah = getpass.getpass('Masukan PIN Anda : ') #norek nasabah untuk memasukan pin rekening nasabah
+    if checkNorek(norekNasabah, pinNasabah) is True:  #jika fungsi checkNorek mengembalikan nilai True, maka..
+      while True:
+        print()
+        tarikTunai = int(input('Masukan nominal yang akan ditarik : Rp. '))
+        if tarikTunai >= 50000:
+          totalSaldo = int(checkNorek.getNasabahSaldo)-tarikTunai
+          currentData = [norekNasabah, checkNorek.getNasabahNama, str(totalSaldo), pinNasabah+'\n']
+          checkNorek.getNasabahData[checkNorek.getNasabahIndex] = ','.join(currentData)
+          openNasabahWrite = open('data/nasabah.txt', 'w')
+          for dataInput in checkNorek.getNasabahData:
+            openNasabahWrite.writelines(dataInput)
+          openNasabahWrite.close()
+          print('Tarik tunai berhasil, total saldo Anda sekarang Rp.', totalSaldo)
+          print('Tekan Enter untuk ke menu')
+          input()
+          index.main()
+          return False
+        else:
+          pass
+    else:
+      print('PIN atau nomor rekening Anda salah')
+
+
+def checkNorek(norekNasabah, pinNasabah): #fungsi checkNorek untuk memvalidasi nomor rekening di dalam nasabah.txt
+  # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+  # nama variabel yang diawali dengan nama fungsi itu sendiri agar variabelnya bisa dipanggil di fungsi lain  #
+  # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+  openNasabah = open('data/nasabah.txt')  #buka file nasabah.txt lalu simpan kedalam variabel openNasabah
+  checkNorek.getNasabahData = openNasabah.readlines() #buat variabel getNasabahData dengan value openNasabah.readlines()
+  checkNorek.getNasabahIndex = 0
+  checkNorek.getNasabahNama = ''
+  checkNorek.getNasabahSaldo = 0
+  for nasabahData in checkNorek.getNasabahData: #loop value dari checkNorek.getNasabahData sebagai nasabahData
+    nasabahData = nasabahData.split(',')  #variabel nasabah data di split dengan ',' lalu simpan kedalam variabel itu sendiri
+    if nasabahData[0] == norekNasabah:  #jika nasabah data index 0 (rekening) sama dengan norekNasabah (input), maka...
+      if nasabahData[3] == pinNasabah+'\n': #jika nasabah data index 3 (rekening) sama dengan pinNasabah (input), maka...
+        checkNorek.getNasabahNama = nasabahData[1]
+        checkNorek.getNasabahSaldo = nasabahData[2]
+        return True #kembalikan nilai True
+      else:
+        pass
+    else:
+      checkNorek.getNasabahIndex += 1
+      pass
+  openNasabah.close() #tutup file openNasabah
